e0ec896013aa7404f186857914b7c1fd494446b4
Aaron Spurgeon <aspurgeon061@gmail.com>
2019-12-10 19:43:44 -0500

    Added a get route to grab all the posts, also added a custom logger middleware that receives the mvp listed as well as a detailed time stamp

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // code away!
+const express = require("express");
+const logger = require("./middleware/logger");
+const postRouter = require("./posts/postRouter");
+
+const server = express();
+
+server.use(logger);
+server.use(express.json());
+
+server.use("/api/posts", postRouter);
+
+server.use((req, res) => {
+  res.status(404).json({
+    message: "Route was not found"
+  });
+});
+
+server.use((err, req, res, next) => {
+  console.log(err);
+  res.status(500).json({
+    message: "An internal error occurred, please try again later."
+  });
+});
+
+server.listen(8080, () => {
+  console.log("\n*** Server Running on http://localhost:8080 ***\n");
+});
--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,20 +1,35 @@
-const express = require('express');
-
+const express = require("express");
+const db = require("./postDb");
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
+  // do your magic!
+  const opts = {
+    limit: req.query.limit,
+    sortby: req.query.sortby,
+    sortdir: req.query.sortdir
+  };
+
+  db.get()
+    .then(post => {
+      res.status(200).json(post);
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: "Error Retrieving post."
+      });
+    });
+});
+
+router.get("/:id", (req, res) => {
   // do your magic!
 });
 
-router.get('/:id', (req, res) => {
+router.delete("/:id", (req, res) => {
   // do your magic!
 });
 
-router.delete('/:id', (req, res) => {
-  // do your magic!
-});
-
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   // do your magic!
 });
 
